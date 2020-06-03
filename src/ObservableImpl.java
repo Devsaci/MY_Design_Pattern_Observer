a0cf399e17c4c4ab66a6ae5c4b65255939e4970f
saci zakaria <sacideveloppeur@gmail.com>
2020-06-03 12:09:31 +0200

    creat interface : Observer, Observable & class Observablempl & List<Obsever>

--- src/ObservableImpl.java
+++ src/ObservableImpl.java
@@ -1,5 +1,9 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class ObservableImpl extends Observable {
 
+    private List<Observer> observers = new ArrayList<>();
 
     @Override
     public void subscribe(Observer observer) {
